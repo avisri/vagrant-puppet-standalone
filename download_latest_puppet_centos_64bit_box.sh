57367028a2d67f580e44c214c91646429561daf9
Avi Sri <avisri@github.com>
2015-04-23 11:01:06 -0700

    works in single run,  takes into account whether things are already downloaded/installed

--- download_latest_puppet_centos_64bit_box.sh
+++ download_latest_puppet_centos_64bit_box.sh
@@ -1,13 +1,26 @@
+#!/bin/bash 
+
+pkgmgr=$(which yum || which apt-get || which brew ) 
+
+[ "${pkgmgr%%brew}" == "$pkgmgr" ]  && pkgmgr="sudo $pkgmgr"
+
+#install deps
+$pkgmgr install lynx vagrant puppet ||  $pkgmgr cask install vagrant puppet 
 
 latest=`lynx -listonly   -dump https://atlas.hashicorp.com/puppetlabs| tr ' ' '\n'   | grep /centos  | grep -v enterprise | grep 64| grep -v  nocm |sort -n   | tail -1`
 
-if [ "$latest" != "" ]
+vagrant box list | grep -q  `echo $latest | tr '/' '\n'| tail -1`
+
+if [ $? -ne 0 ] && [ "$latest" != "" ]
 then
 	
 	vagrant box add $latest
 fi
 
 mkdir -p ~/vagrant/puppet/{manifests,modules}
+puppet module install puppetlabs/firewall --modulepath ~/vagrant/puppet/modules
+
 cd ~/vagrant
-vagrant init
+vagrant up
+vagrant ssh 
 
